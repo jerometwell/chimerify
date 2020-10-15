8d6308171e47e4d42c60a42f87ccfe810cf4d312
Jerome Twell <jtwell1@gmail.com>
2020-10-15 22:55:35 +0100

    explain restructure

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * Use this file to export functions and other library resources
+ * Who wants a library for merging animals? Not my problem!
+ */
 const {getAnimal, mergeAnimals} = require("./lib/animals");
 
 module.exports = {
